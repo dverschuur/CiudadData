ca7a785bbef8c6c6f5937cb2ea70195b9308f9cf
gilberto <ggmoncada.23@est.ucab.edu.ve>
2026-01-05 19:56:28 -0400

    arreglo error

--- src/routes/transitRoutes.ts
+++ src/routes/transitRoutes.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 import { postIncident } from '../controllers/transitController';
 import { validarReporteTransit } from '../middlewares/validarReporteTransit';
-import { validarCiudadTransit } from '../middlewares/validarCiudadTransit';
 
 const router = Router();
 
-router.post('/incident', validarCiudadTransit, validarReporteTransit, postIncident);
+router.post('/incident', validarReporteTransit, postIncident);
 
 export default router;
