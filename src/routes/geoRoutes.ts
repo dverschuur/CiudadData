736a9055662d5263ad2e57af21bcd469e95a302d
Daniel Verschuur <danielverschuur31@gmail.com>
2026-01-05 22:30:35 -0400

    ruta y middleware para el metodo get/population

--- src/routes/geoRoutes.ts
+++ src/routes/geoRoutes.ts
@@ -2,9 +2,12 @@ import { Router } from 'express';
 import * as geoController from '../controllers/geoController';
 import { validarReporte } from '../middlewares/validarReporte';
 import { validarCiudad } from '../middlewares/validarCiudad';
+import { validarPaisPoblacion } from '../middlewares/validarPaisGeo';
 
 const router = Router();
+
 router.post('/report', validarReporte, geoController.postReporteCiudadano);
 router.get('/city/:city', validarCiudad, geoController.getCiudad);
+router.get('/population/:country',validarPaisPoblacion, geoController.getPoblacionPais);
 
 export default router;
